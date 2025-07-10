0e62a5ce190df827781b639071a2bac326d82b4f
Jordan Grieve <grievejordan@yahoo.com>
2025-07-10 15:23:00 +0100

    Create Page UI created

--- 01_MERN-Crash-Course/frontend/src/pages/CreatePage.jsx
+++ 01_MERN-Crash-Course/frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import {
+  Container,
+  Box,
+  VStack,
+  Heading,
+  useColorModeValue,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-function CreatePage() {
-  return <div>CreatePage</div>;
-}
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const handleAddProduct = () => {
+    console.log("New Product Created:", newProduct);
+  };
+
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Description"
+              value={newProduct.description}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, description: e.target.value })
+              }
+            />
+          </VStack>
+          <Button
+            colorScheme="blue"
+            onClick={handleAddProduct}
+            w={"full"}
+            mt={6}
+          >
+            Create Product
+          </Button>
+        </Box>
+      </VStack>
+    </Container>
+  );
+};
 
 export default CreatePage;
