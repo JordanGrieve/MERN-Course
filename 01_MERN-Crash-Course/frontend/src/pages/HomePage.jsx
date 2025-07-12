104675d139c03776eeff2cdffd2b91404bbf4bac
Jordan Grieve <grievejordan@yahoo.com>
2025-07-12 08:57:24 +0100

    HomePage.jsx created and dynamically fetches relevant products with Chakra UI

--- 01_MERN-Crash-Course/frontend/src/pages/HomePage.jsx
+++ 01_MERN-Crash-Course/frontend/src/pages/HomePage.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react";
+import { use, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
+import ProductCard from "../components/ProductCard";
 
-function HomePage() {
-  return <div>HomePage</div>;
-}
+const homePage = () => {
+  const { fetchProducts, products } = useProductStore();
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log(products);
 
-export default HomePage;
+  return (
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.500, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products
+        </Text>
+
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} w={"full"}>
+          {/* Map through your products and display them here */}
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
+
+        {products.length === 0 && (
+          <Text
+            fontSize={"xl"}
+            fontWeight={"bold"}
+            color={"gray.500"}
+            textAlign={"center"}
+          >
+            No Products Available 😢 {""}
+            <Link to={"/create"}>
+              <Text
+                as="span"
+                color="blue.500"
+                _hover={{ textDecoration: "underline" }}
+              >
+                Create Product here.
+              </Text>
+            </Link>
+          </Text>
+        )}
+      </VStack>
+    </Container>
+  );
+};
+export default homePage;
