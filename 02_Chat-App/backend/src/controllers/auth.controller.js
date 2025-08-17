4a369015619caa314c0d8e3aa9e2a225fa94f21c
Jordan Grieve <grievejordan@yahoo.com>
2025-08-17 14:34:44 +0100

    Added the ability to update a users profile picture with the help of cloudinary services.

--- 02_Chat-App/backend/src/controllers/auth.controller.js
+++ 02_Chat-App/backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { generateToken } from '../lib/utils.js';
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -85,5 +86,30 @@ export const logout = (req, res) => {
   } catch (error) {
     console.log('Error in signup controller', error);
     return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    // Able to get becuase we added this in the auth,middleware.js (request)
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: 'Profile Pic requried' });
+    }
+
+    const uploadResponse = await cloudinary.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log('error in update profile', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
