50b6c04b232b27ff1d3a6fa8718af812b552f8f2
Jordan Grieve <grievejordan@yahoo.com>
2025-08-17 19:50:33 +0100

    Sign up page created

--- 02_Chat-App/frontend/src/store/useAuthStore.js
+++ 02_Chat-App/frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { axiosInstance } from '../lib/axios.js';
+import toast from 'react-hot-toast';
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -19,6 +20,29 @@ export const useAuthStore = create((set) => ({
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post('/auth/signup', data);
+      set({ authUser: res.data });
+      toast.success('account created successfully');
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post('/auth/logout');
+      set({ authUser: null });
+      toast.success('account logged out successfully');
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   },
 }));
