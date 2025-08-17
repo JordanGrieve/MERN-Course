58d3163c58975f9ecc77c20818717add944e0982
Jordan Grieve <grievejordan@yahoo.com>
2025-08-17 13:31:06 +0100

    Added the ability to login and logout

--- 02_Chat-App/backend/src/controllers/auth.controller.js
+++ 02_Chat-App/backend/src/controllers/auth.controller.js
@@ -46,14 +46,44 @@ export const signup = async (req, res) => {
     }
   } catch (error) {
     console.log('Error in signup controller', error);
-    res.status(500).json({ message: 'Internal Server Error HERER' });
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
-export const login = (req, res) => {
-  res.send('login route');
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid Credentials' });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'Invalid Credentials' });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log('Error in signup controller', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send('logout route');
+  try {
+    res.cookie('jwt', '', { maxAge: 0 });
+    console.log('Logged Out');
+    return res.status(200).json({ message: 'Logged out successfully' });
+  } catch (error) {
+    console.log('Error in signup controller', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
