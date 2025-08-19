eb81672904523d4875e696af5afaff22b9563871
Jordan Grieve <grievejordan@yahoo.com>
2025-08-19 21:25:57 +0100

    Sidebar added

--- 02_Chat-App/frontend/src/pages/HomePage.jsx
+++ 02_Chat-App/frontend/src/pages/HomePage.jsx
@@ -1,5 +1,22 @@
+import { useChatStore } from '../store/useChatStore.js';
+import { Sidebar } from 'lucide-react';
+import ChatContainer from '../components/ChatContainer.jsx';
+import NoChatSelected from '../components/NoChatSelected.jsx';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { selectedUser } = useChatStore();
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h[cal(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
