1f87c0ce077465de55b5210c2e0d61c52d83c89e
Jordan Grieve <grievejordan@yahoo.com>
2025-09-20 22:38:44 +0100

    user and transaction models created

---
+++ 04-Expenses-Tracker/backend/models/transaction.model.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  description: { type: String, required: true },
+  paymentType: { type: String, enum: ['cash', 'card'], required: true },
+  category: { type: String, required: true },
+  amount: { type: Number, enum: ['saving', 'expense', 'investment'], required: true },
+  location: { type: String, default: 'Unknown' },
+  date: { type: Date, required: true },
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+export default Transaction;
