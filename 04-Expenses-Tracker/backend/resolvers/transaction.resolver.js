e18e66857e4a0e924964cf4166523e29cfa8e7f6
Jordan Grieve <grievejordan@yahoo.com>
2025-09-21 21:26:40 +0100

    transaction model, type def and resolver added

--- 04-Expenses-Tracker/backend/resolvers/transaction.resolver.js
+++ 04-Expenses-Tracker/backend/resolvers/transaction.resolver.js
@@ -1,29 +1,59 @@
 import Transaction from '../models/transaction.model.js';
+import User from '../models/user.model.js';
 
-const transactionResolvers = {
+const transactionResolver = {
   Query: {
-    transactions: async (_, context) => {
+    transactions: async (_, __, context) => {
       try {
-        if (!context.getUser()) throw new Error('Unauthenticated');
+        if (!context.getUser()) throw new Error('Unauthorized');
         const userId = await context.getUser()._id;
 
         const transactions = await Transaction.find({ userId });
         return transactions;
-      } catch (e) {
-        console.log('Error fetching transactions:', e);
-        throw new Error(e);
+      } catch (err) {
+        console.error('Error getting transactions:', err);
+        throw new Error('Error getting transactions');
       }
     },
     transaction: async (_, { transactionId }) => {
       try {
         const transaction = await Transaction.findById(transactionId);
         return transaction;
-      } catch (e) {
-        console.log('Error fetching transaction:', e);
-        throw new Error(e);
+      } catch (err) {
+        console.error('Error getting transaction:', err);
+        throw new Error('Error getting transaction');
       }
     },
-    // TODO add category based transaction fetching
+    categoryStatistics: async (_, __, context) => {
+      if (!context.getUser()) throw new Error('Unauthorized');
+
+      const userId = context.getUser()._id;
+      const transactions = await Transaction.find({ userId });
+      const categoryMap = {};
+
+      // const transactions = [
+      // 	{ category: "expense", amount: 50 },
+      // 	{ category: "expense", amount: 75 },
+      // 	{ category: "investment", amount: 100 },
+      // 	{ category: "saving", amount: 30 },
+      // 	{ category: "saving", amount: 20 }
+      // ];
+
+      transactions.forEach((transaction) => {
+        if (!categoryMap[transaction.category]) {
+          categoryMap[transaction.category] = 0;
+        }
+        categoryMap[transaction.category] += transaction.amount;
+      });
+
+      // categoryMap = { expense: 125, investment: 100, saving: 50 }
+
+      return Object.entries(categoryMap).map(([category, totalAmount]) => ({
+        category,
+        totalAmount,
+      }));
+      // return [ { category: "expense", totalAmount: 125 }, { category: "investment", totalAmount: 100 }, { category: "saving", totalAmount: 50 } ]
+    },
   },
   Mutation: {
     createTransaction: async (_, { input }, context) => {
@@ -33,32 +63,45 @@ const transactionResolvers = {
           userId: context.getUser()._id,
         });
         await newTransaction.save();
-      } catch (e) {
-        console.log('Error creating transaction:', e);
-        throw new Error(e);
+        return newTransaction;
+      } catch (err) {
+        console.error('Error creating transaction:', err);
+        throw new Error('Error creating transaction');
       }
     },
     updateTransaction: async (_, { input }) => {
       try {
-        const updateTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, {
+        const updatedTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, {
           new: true,
         });
-        return updateTransaction;
-      } catch (e) {
-        console.log('Error updating transaction:', e);
-        throw new Error(e);
+        return updatedTransaction;
+      } catch (err) {
+        console.error('Error updating transaction:', err);
+        throw new Error('Error updating transaction');
       }
     },
     deleteTransaction: async (_, { transactionId }) => {
       try {
-        const deleteTransactoion = await Transaction.findByIdAndDelete(transactionId);
-        return deleteTransactoion;
-      } catch (e) {
-        console.log('Error deleting transaction:', e);
-        throw new Error(e);
+        const deletedTransaction = await Transaction.findByIdAndDelete(transactionId);
+        return deletedTransaction;
+      } catch (err) {
+        console.error('Error deleting transaction:', err);
+        throw new Error('Error deleting transaction');
       }
     },
   },
-  // TODO add transaction / user relationship
+  Transaction: {
+    user: async (parent) => {
+      const userId = parent.userId;
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (err) {
+        console.error('Error getting user:', err);
+        throw new Error('Error getting user');
+      }
+    },
+  },
 };
-export default transactionResolvers;
+
+export default transactionResolver;
--- 04-Expenses-Tracker/backend/typeDefs/transaction.typeDef.js
+++ 04-Expenses-Tracker/backend/typeDefs/transaction.typeDef.js
@@ -1,5 +1,5 @@
 const transactionTypeDef = `#graphql
-type Transaction {
+  type Transaction {
     _id: ID!
     userId: ID!
     description: String!
@@ -8,36 +8,44 @@ type Transaction {
     amount: Float!
     location: String
     date: String!
-}
-type Query {
+    user: User!
+  }
+
+  type Query {
     transactions: [Transaction!]
-    transaction(transactionId: ID!): Transaction
-  # TODO add category based transaction fetching
-}
-type Mutation {
-  createTransaction(input: CreateTransactionInput!): Transaction!
-  updateTransaction(input: UpdateTransactionInput!): Transaction!
-  deleteTransaction(transactionId: ID!): Transaction!
-}
+    transaction(transactionId:ID!): Transaction
+    categoryStatistics: [CategoryStatistics!]
+  }
 
-input CreateTransactionInput {
-  description: String!
-  paymentType: String!
-  category: String!
-  amount: Float!
-  location: String
-  date: String!
-}
+  type Mutation {
+    createTransaction(input: CreateTransactionInput!): Transaction!
+    updateTransaction(input: UpdateTransactionInput!): Transaction!
+    deleteTransaction(transactionId:ID!): Transaction!
+  }
 
-input UpdateTransactionInput {
-  transactionId: ID!
-  description: String
-  paymentType: String
-  category: String
-  amount: Float
-  location: String
-  date: String
-}
+  type CategoryStatistics {
+    category: String!
+    totalAmount: Float!
+  }
+
+  input CreateTransactionInput {
+    description: String!
+    paymentType: String!
+    category: String!
+    amount: Float!
+    date: String!
+    location: String
+  }
+
+  input UpdateTransactionInput {
+    transactionId: ID!
+    description: String
+    paymentType: String
+    category: String
+    amount: Float
+    location: String
+    date: String
+  }
 `;
 
 export default transactionTypeDef;
