f82dc8bf6362badad4615a5b3427e56cedca55f0
Jordan Grieve <grievejordan@yahoo.com>
2025-10-05 10:36:47 +0100

    Update routing to redirect all unmatched paths to NotFound page

--- 05_GraphQL-Crash-Course/client/src/App.js
+++ 05_GraphQL-Crash-Course/client/src/App.js
@@ -24,7 +24,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/projects/:id" element={<Project />} />
-              <Route path="/dsd" element={<NotFound />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
         </Router>
