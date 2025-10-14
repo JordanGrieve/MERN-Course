4d7f959d6d25454a0a4e48b04b8f865dfa246c08
Jordan Grieve <grievejordan@yahoo.com>
2025-10-14 22:38:02 +0100

    Enhance Navbar with color mode toggle and logo integration

--- 07_best-friend-app/frontend/src/components/Navbar.jsx
+++ 07_best-friend-app/frontend/src/components/Navbar.jsx
@@ -1,20 +1,51 @@
-import { Box, Container, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Container maxW={"900px"}>
-      <Box px={4} my={4} borderRadius={5}>
+      <Box
+        px={4}
+        my={4}
+        borderRadius={5}
+        bg={useColorModeValue("gray.200", "gray.700")}
+      >
         <Flex h="16" alignItems={"center"} justify={"space-between"}>
           {/*  Left Side */}
-          LEf
-        </Flex>
-        <Flex
-          alignItems={"center"}
-          justify={"space-between"}
-          gap={3}
-          display={{ base: "none", sm: "flex" }}
-        >
-          RIGFHT
+          <Flex
+            alignItems={"center"}
+            justify={"space-between"}
+            gap={3}
+            display={{ base: "none", sm: "flex" }}
+          >
+            <img src="/react.png" alt="logo" width={50} height={50} />
+            <Text fontSize={"40px"}>+</Text>
+            <img src="/python.png" alt="logo" width={50} height={40} />
+            <Text fontSize={"40px"}>=</Text>
+            <img src="/explode.png" alt="logo" width={45} height={45} />
+          </Flex>
+          <Flex gap={3} alignItems={"center"}>
+            <Text
+              fontSize={"lg"}
+              fontWeight={500}
+              display={{ base: "none", sm: "block" }}
+            >
+              Best Friend App
+            </Text>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
+            </Button>
+          </Flex>
         </Flex>
       </Box>
     </Container>
