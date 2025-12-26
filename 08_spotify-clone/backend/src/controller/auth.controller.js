885cc6e791933f712c48c9a2f9cf7a8c5a6cb8a6
Jordan Grieve <grievejordan@yahoo.com>
2025-12-26 14:18:29 +0000

    feat: implement authentication callback and user signup logic

---
+++ 08_spotify-clone/backend/src/controller/auth.controller.js
@@ -0,0 +1,22 @@
+import { User } from "../models/user.model.js";
+
+export const authCallBack = async (req, res) => {
+  try {
+    const { id, firstName, lastName, imageUrl } = req.body;
+
+    // Check if user exists in the database
+    const user = await User.findOne({ clerkId: id });
+
+    if (!user) {
+      // Sign up new user
+      await User.create({
+        clerkId: id,
+        fullName: `${firstName} ${lastName}`,
+        imageUrl,
+      });
+      res.status(200).json({ message: "User signed up successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
