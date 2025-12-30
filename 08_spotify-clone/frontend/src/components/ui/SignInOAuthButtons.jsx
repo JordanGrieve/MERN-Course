849f272e69d7ccf6e936a5fd08382c1491289719
Jordan Grieve <grievejordan@yahoo.com>
2025-12-30 21:25:29 +0000

    feat: add authentication flow with SSO callback, enhance AuthCallbackPage UI, and implement SignInOAuthButtons component

---
+++ 08_spotify-clone/frontend/src/components/ui/SignInOAuthButtons.jsx
@@ -0,0 +1,30 @@
+import { useSignIn } from "@clerk/clerk-react";
+import { Button } from "./button";
+
+const SignInOAuthButtons = () => {
+  const { signIn, isLoaded } = useSignIn();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  const SignInWithGoogle = () => {
+    signIn.authenticateWithRedirect({
+      strategy: "oauth_google",
+      redirectUrl: "/sso-callback",
+      redirectUrlComplete: "/auth-callback",
+    });
+  };
+
+  return (
+    <Button
+      onClick={SignInWithGoogle}
+      variant={"secondary"}
+      className="2-full text-white border-zinc-200  h-11"
+    >
+      Sign in with Google
+    </Button>
+  );
+};
+
+export default SignInOAuthButtons;
