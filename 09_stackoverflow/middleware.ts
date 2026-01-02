fb15abe082948338dd31b1eef940b95e730fd87a
Jordan Grieve <grievejordan@yahoo.com>
2026-01-02 15:42:24 +0000

    feat:  Storage management and integrate Zustand for authentication state management

---
+++ 09_stackoverflow/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+import getOrCreateDB from "./models/server/dbSetup";
+import getOrCreateStorage from "./models/server/storageSetup";
+
+export async function middleware(request: NextRequest) {
+  await Promise.all([getOrCreateDB(), getOrCreateStorage()]);
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  // match all request paths except for the ones starting with: /api, /_next, /static, and /favicon.ico
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
